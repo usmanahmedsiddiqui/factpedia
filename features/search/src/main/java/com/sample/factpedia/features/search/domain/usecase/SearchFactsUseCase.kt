5a2f77dfed6b02f4d81df3cbd847eff3196f926a
usman.siddiqui <usman.siddiqui@free-now.com>
2025-06-24 12:53:18 +0200

    fix bug in search

--- features/search/src/main/java/com/sample/factpedia/features/search/domain/usecase/SearchFactsUseCase.kt
+++ features/search/src/main/java/com/sample/factpedia/features/search/domain/usecase/SearchFactsUseCase.kt
@@ -2,7 +2,9 @@ package com.sample.factpedia.features.search.domain.usecase
 
 import com.sample.factpedia.core.common.result.DataError
 import com.sample.factpedia.core.common.result.Response
+import com.sample.factpedia.core.data.model.asEntity
 import com.sample.factpedia.core.domain.model.Fact
+import com.sample.factpedia.database.dao.FactDao
 import com.sample.factpedia.features.search.data.repository.SearchRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
@@ -11,12 +13,16 @@ import javax.inject.Inject
 
 class SearchFactsUseCase @Inject constructor(
     private val searchRepository: SearchRepository,
+    private val factDao: FactDao,
 ) {
     suspend operator fun invoke(query: String): Flow<Response<List<Fact>, DataError>> {
         return when (val result = searchRepository.search(query)) {
             is Response.Failure -> flowOf(Response.Failure(result.error))
-            is Response.Success -> searchRepository.observeFactsWithBookmarks(result.data.map { it.id })
-                .map { Response.Success(it) }
+            is Response.Success -> {
+                factDao.upsertFacts(result.data.map { it.asEntity() })
+                searchRepository.observeFactsWithBookmarks(result.data.map { it.id })
+                    .map { Response.Success(it) }
+            }
         }
     }
 }
